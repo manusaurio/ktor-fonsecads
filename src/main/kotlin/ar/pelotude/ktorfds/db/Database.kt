c6d387bb323d2e0dcfcdb60edbe0d9d1e2244530
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-09-17 18:14:44 -0300

    Implement db+schema. Splitting commits...

---
+++ src/main/kotlin/ar/pelotude/ktorfds/db/Database.kt
@@ -0,0 +1,131 @@
+package ar.pelotude.ktorfds.db
+
+import ar.pelotude.ktorfds.models.GeoMessage
+import ar.pelotude.ktorfds.models.Location
+import org.sqlite.SQLiteConfig
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+class Database: MessagesDatabase<Long> {
+    // TODO: inject dependency and/or use an env variable
+    private val dbUrl = "jdbc:sqlite:messages.db"
+
+    private val config = SQLiteConfig().apply {
+        enforceForeignKeys(true)
+        enableLoadExtension(true)
+        setJournalMode(SQLiteConfig.JournalMode.WAL)
+        busyTimeout = 10_000
+    }.toProperties()
+
+    private fun Connection.createSchema() = createStatement().use { statement ->
+        sequenceOf(
+            "SELECT load_extension('mod_spatialite');",
+            "SELECT InitSpatialMetaData();",
+            """
+            CREATE TABLE message(
+              id INTEGER NOT NULL PRIMARY KEY,
+              author_id INTEGER NOT NULL,
+              content INTEGER NOT NULL,
+              deleted INTEGER NOT NULL DEFAULT 0,
+              creation_time INTEGER NOT NULL DEFAULT(UNIXEPOCH()),
+              level INTEGER NOT NULL DEFAULT 0,
+              FOREIGN KEY(author_id) REFERENCES user(id)
+            );
+            """,
+            "SELECT AddGeometryColumn('message', 'coordinates', 4326, 'POINT', 'XY');",
+            """
+            CREATE TABLE user(
+              id INTEGER NOT NULL PRIMARY KEY,
+              creation_date INTEGER NOT NULL DEFAULT(UNIXEPOCH()),
+              banned INTEGER DEFAULT 0
+            );
+            """,
+            """
+            CREATE TABLE like(
+              message_id INTEGER NOT NULL,
+              author_id INTEGER NOT NULL,
+              grade INTEGER NOT NULL, -- -1, 0 or 1
+              UNIQUE(message_id, author_id),
+              FOREIGN KEY(message_id) REFERENCES message(id),
+              FOREIGN KEY(author_id) REFERENCES user(id)
+            );
+            """,
+            // idk if I'm gonna use these tables but just in case:
+            """
+            CREATE TABLE message_part(
+              type INTEGER NOT NULL, -- 0 template 1 filling 2 conjunction
+              content TEXT NOT NULL,
+              value INTEGER NOT NULL,
+              filling_category_id INTEGER,
+              UNIQUE(type, content, value),
+              PRIMARY KEY(type, value),
+              FOREIGN KEY(filling_category_id) REFERENCES filling_cateogry(id)
+            );
+            """,
+            """
+            CREATE TABLE filling_category(
+              id INTEGER NOT NULL PRIMARY KEY,
+              name TEXT UNIQUE NOT NULL
+            );
+            """
+        ).forEach(statement::execute)
+
+        if (!autoCommit) commit()
+    }
+
+    init {
+        try {
+            DriverManager.getConnection(dbUrl, config)?.use { conn ->
+                conn.autoCommit = false
+
+                conn.createStatement().use { s ->
+                    val res = s.executeQuery("PRAGMA user_version;")
+                    conn.commit()
+
+                    val userVersion = res.getInt(1)
+
+                    if (userVersion == 0) {
+                        conn.createSchema()
+                        s.executeUpdate("PRAGMA user_version = 1;")
+                        conn.commit()
+                    }
+                }
+            }
+        } catch (e: SQLException) {
+            // TODO:
+            throw e
+        }
+    }
+
+    override suspend fun getMessage(id: Long, requesterId: Long): GeoMessage<Long>? {
+        TODO("")
+    }
+
+    override suspend fun findMessages(
+        requesterId: Long,
+        origin: Location?,
+        maxDistance: Double?,
+        since: Long?,
+        limit: Int,
+        vararg ids: Long,
+    ): Collection<GeoMessage<Long>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteMessage(id: Long): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun vote(messageId: Long, userId: Long, vote: Vote): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addMessage(
+        userId: Long,
+        location: Location,
+        content: Long,
+    ): GeoMessage<Long>? {
+        TODO("Not yet implemented")
+    }
+}
