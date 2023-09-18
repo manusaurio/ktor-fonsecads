18c73b3e88af0b217902277e494e91ea0b6f011e
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-09-18 17:46:40 -0300

    Organize plugins

--- src/main/kotlin/ar/pelotude/ktorfds/Application.kt
+++ src/main/kotlin/ar/pelotude/ktorfds/Application.kt
@@ -1,6 +1,8 @@
 package ar.pelotude.ktorfds
 
+import ar.pelotude.ktorfds.plugins.configureMonitoring
 import ar.pelotude.ktorfds.plugins.configureRouting
+import ar.pelotude.ktorfds.plugins.configureSerialization
 import io.ktor.server.application.*
 
 fun main(args: Array<String>): Unit =
@@ -8,5 +10,7 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
     configureRouting()
 }
